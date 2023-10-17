7f178c8d86f0d51c830439703d91a4392e92d83d
Dimitrije Stojanovic <dimitrije.stojanovic@vega.local>
2023-10-17 16:19:30 +0200

    add mochawesome reported, add baseUrl

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,6 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-
+import 'cypress-mochawesome-reporter/register';
 // Alternatively you can use CommonJS syntax:
 require('./commands')
