5938037cfaf139eb8ffe39538b4a524e91656c76
Dimitrije Stojanovic <dimitrije.stojanovic@vega.local>
2023-10-17 15:30:05 +0200

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
     baseUrl: 'https://reqres.in/'
